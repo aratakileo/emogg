9a29ec97f6ee87b0006a392d7889af7627f7a9ec
TeaCondemns <83653555+aratakileo@users.noreply.github.com>
2023-06-26 00:30:24 +0300

    Update StringUtil.java

--- src/main/java/pextystudios/emogg/util/StringUtil.java
+++ src/main/java/pextystudios/emogg/util/StringUtil.java
@@ -3,25 +3,12 @@ package pextystudios.emogg.util;
 import net.minecraft.resources.ResourceLocation;
 
 public final class StringUtil {
-    public static String lstrip(String source, char stripChar) {
-        return source.replaceAll("^" + stripChar + "+", "");
-    }
-
-    public static String rstrip(String source, char stripChar) {
-        return source.replaceAll(stripChar + "+$", "");
-    }
-
     public static String replaceStartEndIndex(String source, int start, int end, Character newChar) {
         return source.substring(0, start) + newChar + source.substring(end);
     }
 
     public static String replaceStartEndIndex(String source, int start, int end, String newSubstring) {
         return source.substring(0, start) + newSubstring + source.substring(end);
-    }
-
-    public static String strip(String source, char stripChar) {
-        return source.replaceAll("^" + stripChar + "+", "")
-                .replaceAll(stripChar + "+$", "");
     }
 
     public static String repr(ResourceLocation resourceLocation) {
