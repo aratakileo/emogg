0101732d23b0a39a821a7e6ba7686f060afcf6f1
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2023-08-09 21:04:04 +0300

    Implement an update check
    Implemented an interface for checking for updates and notifying about them

--- src/main/java/pextystudios/emogg/Emogg.java
+++ src/main/java/pextystudios/emogg/Emogg.java
@@ -10,14 +10,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pextystudios.emogg.emoji.handler.EmojiHandler;
 
+import java.util.Objects;
+
 
 public class Emogg implements ClientModInitializer {
     public static Logger LOGGER = LoggerFactory.getLogger(Emogg.class);
 
     public static final String NAMESPACE = "emogg";
 
+    public static boolean hasMessageAboutUpdateBeenShown = false;
+
     @Override
     public void onInitializeClient() {
+        LOGGER.info(String.format(
+                "emogg client v%s, emogg modrinth v%s - %s",
+                getVersion(),
+                Objects.requireNonNullElse(ModrinthUpdateChecker.getUpdateVersion(), "-unknown"),
+                ModrinthUpdateChecker.needsToBeUpdated() ? "needs to be updated": "not needs to be updated"
+        ));
+
         new EmojiHandler();
 
         EmoggConfig.load();
@@ -33,5 +44,9 @@ public class Emogg implements ClientModInitializer {
                 Component.translatable(String.format("emogg.resourcepack.%s.name", resourcepackName)),
                 ResourcePackActivationType.DEFAULT_ENABLED
         );
+    }
+
+    public static String getVersion() {
+        return FabricLoader.getInstance().getModContainer(NAMESPACE).get().getMetadata().getVersion().getFriendlyString();
     }
 }
--- src/main/java/pextystudios/emogg/mixin/MinecraftMixin.java
+++ src/main/java/pextystudios/emogg/mixin/MinecraftMixin.java
@@ -2,14 +2,31 @@ package pextystudios.emogg.mixin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.font.FontManager;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.locale.Language;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pextystudios.emogg.Emogg;
+import pextystudios.emogg.ModrinthUpdateChecker;
 import pextystudios.emogg.emoji.font.EmojiFont;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
+    @Shadow
+    public ClientLevel level;
+
+    @Shadow @Final
+    public Gui gui;
+
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/font/FontManager;createFont()Lnet/minecraft/client/gui/Font;"))
     private Font createFontWhenInit(FontManager fontManager) {
         return new EmojiFont(fontManager.createFont());
@@ -18,5 +35,13 @@ public class MinecraftMixin {
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/font/FontManager;createFontFilterFishy()Lnet/minecraft/client/gui/Font;"))
     private Font createFontFilterFishyWhenInit(FontManager fontManager) {
         return new EmojiFont(fontManager.createFontFilterFishy());
+    }
+
+    @Inject(method = "setScreen", at = @At("HEAD"))
+    private void setScreen(Screen screen, CallbackInfo ci) {
+        if (screen == null && level != null && !Emogg.hasMessageAboutUpdateBeenShown && ModrinthUpdateChecker.needsToBeUpdated()) {
+            gui.getChat().addMessage(Component.literal("§7[§cemogg§7]§r " + Language.getInstance().getOrDefault("emogg.message.new_version_is_available")));
+            Emogg.hasMessageAboutUpdateBeenShown = true;
+        }
     }
 }
