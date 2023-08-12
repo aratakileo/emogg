34e02c156757e33e03848c067cf9229de955c5b7
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2023-08-12 18:26:34 +0300

    New "new verison is available" message

--- src/main/java/pextystudios/emogg/mixin/MinecraftMixin.java
+++ src/main/java/pextystudios/emogg/mixin/MinecraftMixin.java
@@ -1,5 +1,7 @@
 package pextystudios.emogg.mixin;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.SharedConstants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.Gui;
@@ -8,9 +10,12 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import org.apache.commons.io.IOUtils;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -19,8 +24,16 @@ import pextystudios.emogg.Emogg;
 import pextystudios.emogg.api.ModrinthApi;
 import pextystudios.emogg.font.EmojiFont;
 
+import java.nio.charset.Charset;
+
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
+    @Unique
+    private final static ResourceLocation messageResourceLocation = new ResourceLocation(
+            Emogg.NAMESPACE_OR_ID,
+            "rawtext/new_version_is_available.json"
+    );
+
     @Shadow
     public ClientLevel level;
 
@@ -45,12 +58,36 @@ public class MinecraftMixin {
                         && !Emogg.hasMessageAboutUpdateBeenShown
                         && ModrinthApi.needsToBeUpdated()
         ) {
-            gui.getChat().addMessage(Component.literal(
-                    "§7[§cemogg§7]§r "
-                            + Language.getInstance().getOrDefault("emogg.message.new_version_is_available")
-            ));
+            try {
+                final var lang = Language.getInstance();
+                final var rightMessageParts = lang.getOrDefault("emogg.message.new_version_is_available")
+                        .split("%s");
+                final var messageRawtext = IOUtils.toString(
+                        Minecraft.getInstance()
+                                .getResourceManager()
+                                .getResource(messageResourceLocation)
+                                .get()
+                                .open(),
+                                Charset.defaultCharset()
+                ).replaceAll(
+                        "\\{link}",
+                                "https://modrinth.com/mod/emogg/versions?g="
+                                        + SharedConstants.getCurrentVersion().getName()
+                        ).replaceAll("\\{tooltip}", lang.getOrDefault("chat.link.open"))
+                        .replace("{left}", rightMessageParts[0])
+                        .replace("{right}", rightMessageParts.length > 1 ? rightMessageParts[1] : "")
+                        .replace("{button}", "Modrinth");
 
-            Emogg.hasMessageAboutUpdateBeenShown = true;
+                gui.getChat().addMessage(Component.Serializer.fromJson(messageRawtext));
+
+                Emogg.hasMessageAboutUpdateBeenShown = true;
+            } catch (Exception e) {
+                gui.getChat().addMessage(
+                        Component.literal("Emogg: something went wrong...")
+                                .withStyle(style -> style.withColor(ChatFormatting.RED))
+                );
+                Emogg.LOGGER.error("Trouble: ", e);
+            }
         }
     }
 }
