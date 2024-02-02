121c243e9caaaf10002152fa43f5103c632c6760
AratakiLeo <83653555+aratakileo@users.noreply.github.com>
2024-02-02 19:24:17 +0300

    Changing the debugging GUI
    Now the additional debugging buttons on the settings screen are available only when debugging mode is enabled

--- src/main/java/io/github/aratakileo/emogg/gui/screen/SettingsScreen.java
+++ src/main/java/io/github/aratakileo/emogg/gui/screen/SettingsScreen.java
@@ -13,10 +13,15 @@ import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 @Environment(EnvType.CLIENT)
 public class SettingsScreen extends AbstractScreen {
+    private final @Nullable List<Button> debugButtons = new ArrayList<>();
+
     public SettingsScreen() {
         super(Component.translatable("emogg.settings.title"));
     }
@@ -28,54 +33,100 @@ public class SettingsScreen extends AbstractScreen {
     @Override
     protected void init() {
         int y = 40 - 20;
-        addBooleanConfigButton("enableDebugMode", "enable_debug_mode", y+=20);
-        addBooleanConfigButton("enableAtlasDebugHUD", "enable_atlas_debug_hud", y+=20);
-        addRenderableWidget(new Button(
+        addConfigToggleButton(
+                "enableDebugMode",
+                "enable_debug_mode",
+                y+=20,
+                this::onToggleDebugMode
+        );
+
+        var debugButton = addConfigToggleButton(
+                "enableAtlasDebugHUD",
+                "enable_atlas_debug_hud",
+                y+=20,
+                null
+        );
+
+        if (Objects.nonNull(debugButton) && Objects.nonNull(debugButtons))
+            debugButtons.add(debugButton);
+
+        debugButton = new Button(
                 0, y+=20,
                 Component.translatable("emogg.settings.option.reload_all.title")) {{
-                    setTooltip(Component.translatable("emogg.settings.option.reload_all.description"));
-                    setOnClicked(button -> EmojiManager.getInstance().getEmojisStream()
-                            .forEach(e -> e.reload(false)));
-                    x = horizontalCenter() - width / 2;
-        }});
-        addRenderableWidget(new Button(
+            setTooltip(Component.translatable("emogg.settings.option.reload_all.description"));
+            setOnClicked(button -> EmojiManager.getInstance().getEmojisStream()
+                    .forEach(e -> e.reload(false)));
+            x = horizontalCenter() - width / 2;
+        }};
+
+        addRenderableWidget(debugButton);
+
+        if (Objects.nonNull(debugButtons))
+            debugButtons.add(debugButton);
+
+        debugButton = new Button(
                 0, y+=20,
                 Component.translatable("emogg.settings.option.force_load_all.title")) {{
-                    setTooltip(Component.translatable("emogg.settings.option.force_load_all.description"));
-                    setOnClicked(button -> EmojiManager.getInstance().getEmojisStream()
-                            .forEach(Emoji::forceLoad));
-                    x = horizontalCenter() - width / 2;
-        }});
+            setTooltip(Component.translatable("emogg.settings.option.force_load_all.description"));
+            setOnClicked(button -> EmojiManager.getInstance().getEmojisStream()
+                    .forEach(Emoji::forceLoad));
+            x = horizontalCenter() - width / 2;
+        }};
+
+        addRenderableWidget(debugButton);
+
+        if (Objects.nonNull(debugButtons))
+            debugButtons.add(debugButton);
 
         addRenderableWidget(new Button(0, 0, Component.translatable("emogg.settings.save_and_quit")) {{
             setOnClicked(button -> onClose());
             x = horizontalCenter() - width / 2;
             y = SettingsScreen.this.height - height - 20;
         }});
+
+        onToggleDebugMode(null);
     }
 
-    private void addBooleanConfigButton(String field, String translation, int y) {
-        if (!(EmoggConfig.getField(field) instanceof Boolean)) {
+    private @Nullable Button addConfigToggleButton(
+            @NotNull String configField,
+            @NotNull String translationKey,
+            int y,
+            @Nullable Consumer<Button> onToggle
+    ) {
+        if (!(EmoggConfig.getField(configField) instanceof Boolean)) {
             Emogg.LOGGER.warn("Failed to create boolean config button: invalid type");
-            return;
+            return null;
         }
-        final var title = "emogg.settings.option."+translation+".title";
-        addRenderableWidget(new Button(
+        final var title = "emogg.settings.option."+translationKey+".title";
+        final var button = new Button(
                 0, y,
-                Component.translatable(title, getBooleanValueText((Boolean) EmoggConfig.getField(field)))) {{
-                    setTooltip(Component.translatable("emogg.settings.option."+translation+".description"));
-                    setOnClicked(button -> {
-                        EmoggConfig.setField(field, ! (Boolean) EmoggConfig.getField(field));
-                        ((Button)button).setMessage(Component.translatable(title, getBooleanValueText((Boolean) EmoggConfig.getField(field))), true);
-                    });
-                    x = horizontalCenter() - width / 2;
-        }});
+                Component.translatable(title, getBooleanValueText((Boolean) EmoggConfig.getField(configField)))) {{
+            setTooltip(Component.translatable("emogg.settings.option."+translationKey+".description"));
+            setOnClicked(button -> {
+                EmoggConfig.setField(configField, ! (Boolean) EmoggConfig.getField(configField));
+                ((Button)button).setMessage(Component.translatable(title, getBooleanValueText((Boolean) EmoggConfig.getField(configField))), true);
+
+                if (Objects.nonNull(onToggle))
+                    onToggle.accept((Button) button);
+            });
+            x = horizontalCenter() - width / 2;
+        }};
+
+        addRenderableWidget(button);
+
+        return button;
     }
 
     private @NotNull String getBooleanValueText(boolean state) {
         return "§l"
                 + (state ? "§2" : "§c")
                 + Language.getInstance().getOrDefault("emogg.gui.state." + (state ? "enabled" : "disabled"));
+    }
+
+    private void onToggleDebugMode(@Nullable Button button /* only for replacing lambda with method reference */) {
+        if (Objects.isNull(debugButtons)) return;
+
+        debugButtons.forEach(btn -> btn.visible = EmoggConfig.instance.enableDebugMode);
     }
 
     @Override
